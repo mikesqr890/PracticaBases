c44ea6c5c0d4c3d9550088e0f0ed028f48b51beb
Mikesqr <maicsqr@gmail.com>
2026-03-16 20:49:13 -0500

    Creacion de aplicativo Base de datos TiendaMysql

---
+++ conexion.py
@@ -0,0 +1,140 @@
+import mysql.connector
+
+try: 
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="12345678",
+        database="tiendaMysql")
+    if conexion.is_connected():
+        print("Conexión exitosa a la tiendaMysql")
+except Exception as e:
+    print(f"Error al conectar a la tiendaMysql: {e}")
+
+cursor = conexion.cursor()
+
+def registrar_cliente(cursor, conexion):
+    nombre = input("Nombre: ")
+    telefono = input("Telefono: ")
+    correo = input("Correo: ")
+
+    sql = "INSERT INTO clientes (nombreCliente, telefono, correo) VALUES (%s, %s, %s)"
+    valores = (nombre, telefono, correo)
+
+    cursor.execute(sql, valores)
+    conexion.commit()
+
+    print("Cliente registrado")
+
+def registrar_producto(cursor, conexion):
+    nombre = input("Nombre del producto: ")
+    precio = float(input("Precio: "))
+    stock= int(input("Stock: "))
+
+    sql = "INSERT INTO productos (nombreProducto, precio, stock) VALUES (%s, %s, %s)"
+    valores = (nombre, precio, stock)
+
+    cursor.execute(sql, valores)
+    conexion.commit()
+
+    print("Producto registrado")
+
+from datetime import datetime
+
+def registrar_venta(cursor, conexion):
+    print("\n--- REGISTRAR VENTA ---")
+
+    # Mostrar clientes
+    cursor.execute("SELECT idCliente, nombreCliente FROM clientes")
+    clientes = cursor.fetchall()
+
+    print("\nClientes disponibles:")
+    for c in clientes:
+        print(f"ID: {c[0]} - Nombre: {c[1]}")
+
+    # Mostrar productos
+    cursor.execute("SELECT idProducto, nombreProducto FROM productos")
+    productos = cursor.fetchall()
+
+    print("\nProductos disponibles:")
+    for p in productos:
+        print(f"ID: {p[0]} - Producto: {p[1]}")
+
+    # Pedir datos
+    id_cliente = int(input("\nIngrese ID del cliente: "))
+    id_producto = int(input("Ingrese ID del producto: "))
+    cantidad = int(input("Cantidad: "))
+
+    # 🔥 VALIDACIÓN CORRECTA
+    while True:
+        fecha_input = input("Fecha (YYYY-MM-DD) o ENTER para hoy: ")
+        try:
+            if fecha_input == "":
+                fecha = datetime.today().date()
+            else:
+                fecha = datetime.strptime(fecha_input, "%Y-%m-%d").date()
+            break
+        except:
+            print("❌ Formato incorrecto. Usa YYYY-MM-DD")
+
+    # Insertar
+    sql = "INSERT INTO ventas (idCliente, idProducto, fecha, cantidad) VALUES (%s, %s, %s, %s)"
+    valores = (id_cliente, id_producto, fecha, cantidad)
+
+    cursor.execute(sql, valores)
+    conexion.commit()
+
+    print(" Venta registrada correctamente")
+
+
+def mostrar_ventas(cursor):
+    print("\n--- LISTADO DE VENTAS ---")
+
+    cursor.execute("""
+    SELECT c.nombreCliente, p.nombreProducto, v.fecha,v.cantidad
+    FROM ventas v
+    JOIN clientes c ON v.idCliente = c.idCliente
+    JOIN productos p ON v.idProducto = p.idProducto
+    """)
+
+    resultados = cursor.fetchall()
+
+    if len(resultados) == 0:
+        print("No hay ventas registradas")
+    else:
+        print("\nCliente | Producto | Cantidad")
+        print("-" * 35)
+        for fila in resultados:
+            print(f"{fila[0]} | {fila[1]} | {fila[2]}")
+
+while True:
+    print("\n--- MENÚ ---")
+    print("1. Registrar cliente")
+    print("2. Registrar producto")
+    print("3. Registrar venta")
+    print("4. Ver ventas")
+    print("5. Salir")
+
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        registrar_cliente(cursor, conexion)
+
+    elif opcion == "2":
+        registrar_producto(cursor, conexion)
+
+    elif opcion == "3":
+        registrar_venta(cursor, conexion)
+
+    elif opcion == "4":
+        mostrar_ventas(cursor)
+
+    elif opcion == "5":
+        print("Saliendo...")
+        break
+
+    else:
+        print("Opción inválida")
+
+cursor.close()
+conexion.close()
